44738ed3b34770623b6f3540027b97c2d6880651
jli103828 <39618498+jli103828@users.noreply.github.com>
2019-07-16 23:48:28 +0200

    Add some summaries, fix some properties

--- ChartJs.Blazor/ChartJS/BarChart/BarChartConfig.cs
+++ ChartJs.Blazor/ChartJS/BarChart/BarChartConfig.cs
@@ -4,11 +4,15 @@ using ChartJs.Blazor.ChartJS.Common.Enums;
 namespace ChartJs.Blazor.ChartJS.BarChart
 {
     /// <summary>
-    /// Config for a bar chart with typesafe data
+    /// Config for a bar chart
     /// <para>Either 'bar' or 'horizontalBar'</para>
     /// </summary>
     public class BarChartConfig : ChartConfigBase<BarChartOptions, BarChartData>
     {
+        /// <summary>
+        /// Creates a new instance of <see cref="BarChartConfig"/>
+        /// </summary>
+        /// <param name="type">Either <see cref="ChartTypes.Bar"/> (default) or <see cref="ChartTypes.HorizontalBar"/></param>
         public BarChartConfig(ChartTypes type = null) : base(type ?? ChartTypes.Bar) { }
     }
 }
--- ChartJs.Blazor/ChartJS/BarChart/BarChartData.cs
+++ ChartJs.Blazor/ChartJS/BarChart/BarChartData.cs
@@ -4,11 +4,21 @@ using ChartJs.Blazor.ChartJS.MixedChart;
 
 namespace ChartJs.Blazor.ChartJS.BarChart
 {
+    /// <summary>
+    /// The data-subconfig of a <see cref="BarChartConfig"/>
+    /// </summary>
     public class BarChartData
     {
-        public List<string> Labels { get; set; } = new List<string>();
+        /// <summary>
+        /// The labels the chart will use. 
+        /// <para>If defined (not null) the x-Axis has to be of type <see cref="Common.Enums.AxisType.Category"/> for the chart to work correctly. 
+        /// You can also you a <see cref="LineChart.Axes.TimeAxis"/> with <see cref="LineChart.TimeTupel{TData}"/> instead of defining labels.</para>
+        /// </summary>
+        public List<string> Labels { get; set; }
 
-        // TODO: implement like LineChartData
-        public List<IMixableDataset<object>> Datasets { get; set; }
+        /// <summary>
+        /// The Datasets displayed in this chart.
+        /// </summary>
+        public HashSet<IMixableDataset<object>> Datasets { get; }
     }
 }
